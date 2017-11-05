8e5a8d81793a125367602fdbb698ff05bc606c03
jr <jr@jirouette.com>
2017-11-05 23:22:32 +0100

    Commands are now handled in a thread by microservices through redis

--- catholingo/catholingo.py
+++ catholingo/catholingo.py
@@ -3,86 +3,45 @@
 
 import os
 import pydle
+import redis
 import speechdb
+import time
+import json
 from speechdb import Speech, Word
+from threading import Thread
 
-class Command(object):
+class CommandListener(Thread):
 	def __init__(self, client):
+		print("init listener")
+		Thread.__init__(self)
 		self.client = client
-		self.command = self.__class__.COMMAND
-		if self.command[0] == "!":
-			self.command = self.command[1:]
 
-	def is_command(self, command):
-		return self.command == command
+	def run(self):
+		r = redis.StrictRedis(host=os.environ.get('REDIS_HOST', 'localhost'), port=int(os.environ.get('REDIS_PORT', 6379)), db=0)
+		self.pubsub = r.pubsub()
+		self.pubsub.subscribe(os.environ.get('CATHOLINGO_REDIS_EXECUTE_CHANNEL', 'catholingo_execute'))
+		while True:
+			message = self.pubsub.get_message()
+			if message:
+				print(message)
+				self.parse_message(message)
+			time.sleep(1)
 
-	def execute(self, source, target, message):
-		self.client.message(source, "hello ! ")
+	def parse_message(self, message):
+		data = message.get('data')
+		if type(data) is bytes:
+			data = json.loads(data.decode('utf-8'))
+			method = data.get('method')
+			args = data.get('args', list())
+			kwargs = data.get('kwargs', dict())
+			if hasattr(self.client, method):
+				print(getattr(self.client, method), args, kwargs)
+				return getattr(self.client, method)(*args, **kwargs)
 
-class TalkativeCommand(Command):
-	def execute(self, source, target, message):
-		response = self.talk(source, target, message)
-		self.client.message(source, response)
-
-	def talk(self, source, target, message):
-		return "hello world ! "
-
-class RandomCommand(TalkativeCommand):
-	COMMAND = "speak"
-
-	def talk(self, source, target, message):
-		word = Word.select().where(Word.previous_word >> None).order_by(speechdb.random()).first()
-		if word:
-			pprev_word = None
-			prev_word  = word.word
-			payload = word.word
-			maxlength = 100
-			while maxlength > 0:
-				maxlength -= 1
-				pprev_cond = speechdb.none_or(Word.pprevious_word, pprev_word)
-				prev_cond = speechdb.none_or(Word.previous_word, prev_word)
-				word = Word.select().where(pprev_cond & prev_cond).order_by(speechdb.random()).first()
-				if word:
-					payload += " " + word.word
-					if word.next_word is None:
-						break
-
-					pprev_word = prev_word
-					prev_word = word.word
-				else:
-					break
-			return payload
-		else:
-			return "no result"
-
-class SpeakforCommand(TalkativeCommand):
-	COMMAND = "speakfor"
-
-	def talk(self, source, target, message):
-		if len(message.split()) < 2:
-			return ":("
-		user = message.split()[1]
-		word = Word.select().join(Speech).where((Word.previous_word >> None) & (Speech.user == user)).order_by(speechdb.random()).first()
-		if word:
-			pprev_word = None
-			prev_word = word.word
-			payload = word.word
-			maxlength = 100
-			while maxlength > 0:
-				maxlength -= 1
-				pprev_cond = speechdb.none_or(Word.pprevious_word, pprev_word)
-				prev_cond = speechdb.none_or(Word.previous_word, prev_word)
-				word = Word.select().join(Speech).where(pprev_cond & prev_cond & (Speech.user == user)).order_by(speechdb.random()).first()
-				if word:
-					payload += " " + word.word
-					if word.next_word is None:
-						break
-					pprev_word = prev_word
-					prev_word = word.word
-				else:
-					break
-			return payload
-		return "nope "+user+" :("
+class CommandOrder(object):
+	def order(self, channel, user, message):
+		r = redis.StrictRedis(host=os.environ.get('REDIS_HOST', 'localhost'), port=int(os.environ.get('REDIS_PORT', 6379)), db=0)
+		return r.publish(os.environ.get('CATHOLINGO_REDIS_ORDER_CHANNEL', 'catholingo_order'), " ".join([channel, user, message]))
 
 class CathoLingo(pydle.Client):
 	def on_connect(self):
@@ -91,34 +50,16 @@ class CathoLingo(pydle.Client):
 		for chan in os.environ.get('CHANNELS', '#amdo').split():
 			self.join(chan)
 
-	def set_commands(self, *args):
-		self.commands = []
-		for command in args:
-			if isinstance(command, Command):
-				self.commands.append(command)
-			elif isinstance(command, type):
-				try:
-					command = command(client)
-					if isinstance(command, Command):
-						self.commands.append(command)
-				except:
-					pass
-
 	def on_message(self, source, target, message):
 		self.save_speech(source, target, message)
 		self.command(source, target, message)
 
-	def talk(self, source, target, message):
-		self.message(source, "cccht, je suis en mission secrète, je reste silencieux")
-
 	def command(self, source, target, message):
+		command = CommandOrder()
 		if "CathoLingo" in message:
-			self.talk(source, target, message)
+			command.order(source, target, "!speak")
 		elif message[0] == "!":
-			cmd = message.split()[0][1:]
-			for command in self.commands:
-				if command.is_command(cmd):
-					return command.execute(source, target, message)
+			command.order(source, target, message)
 
 	def save_speech(self, channel, user, message):
 		if message[0] in ["!", ",", ";", "~", "#", ".", "§"]:
@@ -149,6 +90,6 @@ class CathoLingo(pydle.Client):
 
 if __name__ == '__main__':
 	client = CathoLingo(os.environ.get('USERNAME', 'CathoLingo'), realname=os.environ.get('REALNAME', 'la pizzeria'))
-	client.set_commands(RandomCommand, SpeakforCommand)
 	client.connect(os.environ.get('IRC_HOST', 'chat.freenode.net'), int(os.environ.get('IRC_PORT', 6697)), tls=True, tls_verify=False)
+	CommandListener(client).start()
 	client.handle_forever()
