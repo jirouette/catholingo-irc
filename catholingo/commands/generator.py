f8ccde338b8548df952273170e4f8ff313b7adf1
jr <jr@jirouette.com>
2017-11-19 21:16:48 +0100

    Added commands and !startwith

--- catholingo/commands/generator.py
+++ catholingo/commands/generator.py
@@ -6,38 +6,66 @@ import speechdb
 import time
 import random
 import sys
+from commands import TalkativeCommandOrder, CommandOrderPool
 from speechdb import Word, Speech
 
-class SpeakCommand(commands.TalkativeCommandOrder):
+def _conditions(*args):
+	result = None
+	for cond in args:
+		if not cond:
+			continue
+		result = cond if not result else (result & cond)
+	return result
+
+def generator(conditions=None, first_condition=None, maxlength=100):
+	speechdb.connect()
+	_cond = _conditions(Word.previous_word >> None, conditions, first_condition)
+	word = Word.select().join(Speech).where(_cond).order_by(speechdb.random()).first()
+	if word:
+		pprev_word = None
+		prev_word  = word.word
+		payload = word.word
+		maxlength = 100
+		while maxlength > 0:
+			maxlength -= 1
+			pprev_cond = speechdb.none_or(Word.pprevious_word, pprev_word)
+			prev_cond = speechdb.none_or(Word.previous_word, prev_word)
+			_cond = _conditions(pprev_cond, prev_cond, conditions)
+			word = Word.select().join(Speech).where(_cond).order_by(speechdb.random()).first()
+			if word:
+				payload += " " + word.word
+				if word.next_word is None:
+					break
+
+				pprev_word = prev_word
+				prev_word = word.word
+			else:
+				break
+		speechdb.close()
+		return payload
+	speechdb.close()
+
+class SpeakCommand(TalkativeCommandOrder):
 	COMMAND = "!speak"
 
 	def talk(self, source, target, message):
-		speechdb.connect()
-		word = Word.select().where(Word.previous_word >> None).order_by(speechdb.random()).first()
-		if word:
-			pprev_word = None
-			prev_word  = word.word
-			payload = word.word
-			maxlength = 100
-			while maxlength > 0:
-				maxlength -= 1
-				pprev_cond = speechdb.none_or(Word.pprevious_word, pprev_word)
-				prev_cond = speechdb.none_or(Word.previous_word, prev_word)
-				word = Word.select().where(pprev_cond & prev_cond).order_by(speechdb.random()).first()
-				if word:
-					payload += " " + word.word
-					if word.next_word is None:
-						break
+		sentence = generator()
+		return sentence if sentence else "no result"
 
-					pprev_word = prev_word
-					prev_word = word.word
-				else:
-					break
-			speechdb.close()
-			return payload
-		else:
-			speechdb.close()
-			return "no result"
+class SpeakforCommand(TalkativeCommandOrder):
+	COMMAND = "!speakfor"
+
+	def talk(self, source, target, message):
+		sentence = generator(Speech.user == message[0])
+		return sentence if sentence else "no result"
+
+class StartWithCommand(TalkativeCommandOrder):
+	COMMAND = "!startwith"
+
+	def talk(self, source, target, message):
+		sentence = generator(first_condition=Word.word == message[0])
+		return sentence if sentence else "no result"
 
 if __name__ == '__main__':
-	SpeakCommand().run()
+	pool = CommandOrderPool(commands=[SpeakCommand, SpeakforCommand, StartWithCommand])
+	pool.run()
